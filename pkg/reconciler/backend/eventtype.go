33b427bbb14caca1a205b2e49f7cddeb1aef26ca
Ali Ok <aliok@redhat.com>
2023-11-30 13:43:52 +0300

    Use a custom struct instead of returning as is

--- pkg/reconciler/backend/eventtype.go
+++ pkg/reconciler/backend/eventtype.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"context"
+	"knative.dev/eventing/pkg/apis/eventing/v1beta2"
 	"knative.dev/pkg/logging"
 	"net/http"
 
@@ -10,6 +11,16 @@ import (
 
 	eventinglistersv1beta2 "knative.dev/eventing/pkg/client/listers/eventing/v1beta2"
 )
+
+type EventType struct {
+	Name        string `json:"name"`
+	Namespace   string `json:"namespace"`
+	Type        string `json:"type"`
+	UID         string `json:"uid"`
+	Description string `json:"description,omitempty"`
+	SchemaData  string `json:"schemaData,omitempty"`
+	SchemaURL   string `json:"schemaURL,omitempty"`
+}
 
 func EventTypeListHandler(ctx context.Context, lister eventinglistersv1beta2.EventTypeLister) func(w http.ResponseWriter, req *http.Request) {
 	logger := logging.FromContext(ctx)
@@ -23,10 +34,28 @@ func EventTypeListHandler(ctx context.Context, lister eventinglistersv1beta2.Eve
 			return
 		}
 
-		err = json.NewEncoder(w).Encode(ret)
+		converted := make([]EventType, 0, len(ret))
+		for _, et := range ret {
+			converted = append(converted, convertEventType(et))
+		}
+
+		err = json.NewEncoder(w).Encode(converted)
 		if err != nil {
 			logger.Errorw("Error encoding eventtypes", "error", err)
 			return
 		}
+	}
+}
+
+func convertEventType(et *v1beta2.EventType) EventType {
+	// TODO: more information!
+	return EventType{
+		Name:        et.Name,
+		Namespace:   et.Namespace,
+		Type:        et.Spec.Type,
+		UID:         string(et.UID),
+		Description: et.Spec.Description,
+		SchemaData:  et.Spec.SchemaData,
+		SchemaURL:   et.Spec.Schema.String(),
 	}
 }
