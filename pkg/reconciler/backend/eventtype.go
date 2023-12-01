20b8b5aa8c53fdfaa6f845889fd2eafc30afd1d4
Ali Ok <aliok@redhat.com>
2023-12-01 11:57:19 +0300

    Show proper data

--- pkg/reconciler/backend/eventtype.go
+++ pkg/reconciler/backend/eventtype.go
@@ -2,24 +2,35 @@ package backend
 
 import (
 	"context"
-	"knative.dev/eventing/pkg/apis/eventing/v1beta2"
-	"knative.dev/pkg/logging"
-	"net/http"
-
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/json"
-
+	"knative.dev/eventing/pkg/apis/eventing/v1beta2"
 	eventinglistersv1beta2 "knative.dev/eventing/pkg/client/listers/eventing/v1beta2"
+	"knative.dev/pkg/logging"
+	"net/http"
 )
 
+var ExcludedAnnotations = map[string]struct{}{
+	"kubectl.kubernetes.io/last-applied-configuration": {},
+}
+
+type EventTypeProvider struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	Kind      string `json:"kind"`
+}
+
 type EventType struct {
-	Name        string `json:"name"`
-	Namespace   string `json:"namespace"`
-	Type        string `json:"type"`
-	UID         string `json:"uid"`
-	Description string `json:"description,omitempty"`
-	SchemaData  string `json:"schemaData,omitempty"`
-	SchemaURL   string `json:"schemaURL,omitempty"`
+	Name        string            `json:"name"`
+	Namespace   string            `json:"namespace"`
+	Type        string            `json:"type"`
+	UID         string            `json:"uid"`
+	Description string            `json:"description,omitempty"`
+	SchemaData  string            `json:"schemaData,omitempty"`
+	SchemaURL   string            `json:"schemaURL,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+	Provider    EventTypeProvider `json:"provider,omitempty"`
 }
 
 func EventTypeListHandler(ctx context.Context, lister eventinglistersv1beta2.EventTypeLister) func(w http.ResponseWriter, req *http.Request) {
@@ -48,6 +59,15 @@ func EventTypeListHandler(ctx context.Context, lister eventinglistersv1beta2.Eve
 }
 
 func convertEventType(et *v1beta2.EventType) EventType {
+	provider := EventTypeProvider{}
+	if et.Spec.Reference != nil {
+		provider.Name = et.Spec.Reference.Name
+		provider.Kind = et.Spec.Reference.Kind
+		provider.Namespace = et.Spec.Reference.Namespace
+	}
+
+	// TODO: need deduplication at this level here
+
 	// TODO: more information!
 	return EventType{
 		Name:        et.Name,
@@ -57,5 +77,20 @@ func convertEventType(et *v1beta2.EventType) EventType {
 		Description: et.Spec.Description,
 		SchemaData:  et.Spec.SchemaData,
 		SchemaURL:   et.Spec.Schema.String(),
+		Labels:      et.Labels,
+		Annotations: filterAnnotations(et.Annotations),
+		// TODO: remove?
+		Provider: provider,
 	}
+}
+
+func filterAnnotations(annotations map[string]string) map[string]string {
+	ret := make(map[string]string)
+	for k, v := range annotations {
+		if _, ok := ExcludedAnnotations[k]; ok {
+			continue
+		}
+		ret[k] = v
+	}
+	return ret
 }
